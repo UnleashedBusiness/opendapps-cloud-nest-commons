d6ef081e2d21856dc06a32359c69c19f55c6176f
ShadowsCrypto <ceo@opendapps.cloud>
2024-02-15 22:23:33 +0200

    feat: new http service container

--- src/service/http-services.container.ts
+++ src/service/http-services.container.ts
@@ -1,25 +1,35 @@
 import {
-  DecentralizedEntityHttpService,
-  DeploymentHttpService,
-  HttpServicesContainer,
-  IndexingHttpService,
-  MultiSignProposalHttpService,
-  NftProxyHttpService,
-  TokenAsAServiceDeployerHttpService
+    DecentralizedEntityHttpService,
+    DeploymentHttpService,
+    HttpServicesContainer,
+    IndexingHttpService,
+    MultiSignProposalHttpService,
+    NftProxyHttpService,
+    TokenAsAServiceDeployerHttpService
 } from "@unleashed-business/opendapps-cloud-ts-commons";
-import { Inject, Injectable } from "@nestjs/common";
-import { HTTP_SERVICE_BASE_URL_TOKEN } from "../nest-commons.const";
+import {Inject, Injectable} from "@nestjs/common";
+import {HTTP_SERVICE_BASE_URL_TOKEN} from "../nest-commons.const";
+import {
+    PresaleServiceDeployerHttpService
+} from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/presale-service-deployer-http.service";
+import {BlocktimeHttpService} from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/blocktime-http.service";
+import {
+    StakingAsAServiceDeployerHttpService
+} from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/staking-as-a-service-deployer-http.service";
 
 @Injectable()
 export default class NestHttpServicesContainer extends HttpServicesContainer {
-  constructor(@Inject(HTTP_SERVICE_BASE_URL_TOKEN) baseUrl: string) {
-    super(
-      new DecentralizedEntityHttpService(baseUrl),
-      new TokenAsAServiceDeployerHttpService(baseUrl),
-      new NftProxyHttpService(baseUrl),
-      new MultiSignProposalHttpService(baseUrl),
-      new IndexingHttpService(baseUrl),
-      new DeploymentHttpService(baseUrl)
-    );
-  }
+    constructor(@Inject(HTTP_SERVICE_BASE_URL_TOKEN) baseUrl: string) {
+        super(
+            new DecentralizedEntityHttpService(baseUrl),
+            new TokenAsAServiceDeployerHttpService(baseUrl),
+            new NftProxyHttpService(baseUrl),
+            new MultiSignProposalHttpService(baseUrl),
+            new IndexingHttpService(baseUrl),
+            new DeploymentHttpService(baseUrl),
+            new PresaleServiceDeployerHttpService(baseUrl),
+            new BlocktimeHttpService(baseUrl),
+            new StakingAsAServiceDeployerHttpService(baseUrl),
+        );
+    }
 }
